cdbff53f194674c80ecb19ec44881bb315a0cd78
zainarhn <zainaraihan@gmail.com>
2022-12-09 08:13:55 +0700

    merubah style pada form

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const submit = document.querySelector(".submit");
 const form = document.querySelector(".form");
+const input = document.querySelectorAll(".inpu");
 
 submit.addEventListener("mouseover", () => {
   form.classList.add("hover");
+  submit.classList.add("button");
+  input[0].classList.add("input");
+  input[1].classList.add("input");
 });
 submit.addEventListener("mouseleave", () => {
   form.classList.remove("hover");
+  submit.classList.remove("button");
+  input[0].classList.remove("input");
+  input[1].classList.remove("input");
 });
